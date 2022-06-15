acaceed3582abad81ffaee473a95b22fb9429aee
Debora <91338311+d4andrad@users.noreply.github.com>
2022-06-15 10:57:19 -0400

    Add files via upload

---
+++ quality_check_code_night_warming.R
@@ -0,0 +1,53 @@
+#find the files
+setwd("C:/Users/user/Desktop/github2/thermal-experiments/night_warming_thermallog")
+files <- list.files(pattern = ".csv")
+
+
+my_data <- as.data.frame(fread(sprintf(files[1], skip=10)))
+colnames(my_data)=c("Date", "Time", "Obs", "Program")
+
+my_data <- as.data.frame(read.csv(files[1], skip=10))
+colnames(my_data)=c("Date", "Time", "Obs", "Program")
+
+#set up empty dataframe
+lng=length(files)
+cat_stats=setNames(data.frame(matrix(ncol = 5, nrow = lng)), c("fname", "program_mean",
+                                                               "obs_mean", "program_sd", "obs_sd"))
+
+#run a regression on each file, and categorize as postive or negative slope; add summary stats
+for (i in 1:length(files)){
+  
+  #read in file
+  my_data <- as.data.frame(read.csv(files[i], skip=10))
+  colnames(my_data)=c("Date", "Time", "Obs", "Program")
+  
+  #convert missing data recorded as zeros to NAs
+  my_data$Obs=ifelse(my_data$Obs==0, NA,my_data$Obs)
+  
+  #convert Date and Time to DateTime format
+  my_data$DateTime=as.POSIXct(paste(my_data$Date, my_data$Time), 
+                              format="%Y-%m-%d %H:%M")
+  if (is.na(my_data$DateTime[1])){
+    my_data$DateTime=as.POSIXct(paste(my_data$Date, my_data$Time), 
+                                format="%d/%m/%Y %H:%M")
+  }
+
+  
+  #subset data to initial lenght of 120 time steps
+  index=seq(from=1, to=nrow(my_data), by=60)
+  
+  subset <- data.frame(sub_time=my_data$Time[index],sub_obs_temp=my_data$Obs[index],
+                       sub_set_temp=my_data$Program[index])
+  
+  #add summary stats
+  cat_stats$program_mean[i]=mean(subset$sub_set_temp, na.rm=TRUE)
+  cat_stats$obs_mean[i]=mean(subset$sub_obs_temp, na.rm=TRUE)
+  
+  cat_stats$program_sd[i]=sd(subset$sub_set_temp, na.rm=TRUE)
+  cat_stats$obs_sd[i]=sd(subset$sub_obs_temp, na.rm=TRUE)
+  
+  cat_stats$fname[i]=files[i]
+}
+
+#write file with results
+write.csv(cat_stats, file="cat_stats_night_warming.csv")
