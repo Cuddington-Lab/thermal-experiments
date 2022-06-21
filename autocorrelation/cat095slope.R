a93496deb94ec671eeb6af21f811562e3655b530
d4andrad <d4andrad@uwaterloo.ca>
2022-06-21 12:07:48 -0400

    update

---
+++ autocorrelation/cat095slope.R
@@ -0,0 +1,44 @@
+library('readxl')
+
+#find the files
+setwd("C:/Users/naazb/Documents/code files/Co-op repo/cat_new")
+files <- list.files(pattern = "_095")
+
+#set up empty dataframe
+lng=length(files)
+cat095=setNames(data.frame(matrix(ncol = 3, nrow = lng)), c("fname", "slp", "cat"))
+
+#run a regression on each file, and categorize as postive or negative slope
+for (i in 1:length(files)){
+  
+#read in file
+my_data <- as.data.frame(read_excel(files[i], skip=10, 
+                      col_names=FALSE))
+colnames(my_data)=c("Date", "Time", "Obs", "Program")
+
+#convert missing data recorded as zeros to NAs
+my_data$Obs=ifelse(my_data$Obs==0, NA,my_data$Obs)
+
+#convert Date and Time to DateTime format
+my_data$DateTime=as.POSIXct(paste(my_data$Date, my_data$Time), 
+                          format="%Y-%m-%d %H:%M")
+if (is.na(my_data$DateTime[1])){
+  my_data$DateTime=as.POSIXct(paste(my_data$Date, my_data$Time), 
+                              format="%d/%m/%Y %H:%M")
+}
+
+#regress first 1/4 of data
+l4=length(my_data$Obs)
+lreg=lm(my_data$Obs[1:l4]~my_data$DateTime[1:l4])
+summary(lreg)
+plot(my_data$Obs[1:l4]~my_data$DateTime[1:l4])
+abline(lreg, col="red")
+
+#record slope and categorize as positive or negative
+cat095$fname[i]=files[i]
+cat095$slp[i]=lreg$coef[2]
+cat095$cat[i]=ifelse(lreg$coef[2]>0, "P", "N")
+}
+
+#write file with results
+write.csv(cat095, file="cat095_PorN.csv")
---
+++ historical_temps_analyses/max-min_temps_historical_toronto.R
@@ -0,0 +1,42 @@
+hist <- read.csv("https://raw.githubusercontent.com/Cuddington-Lab/thermal-experiments/main/historical_temps_toronto.csv", header=TRUE, stringsAsFactors = TRUE)
+hist$LOCAL_DATE <- as.Date(hist$LOCAL_DATE, format = "%m/%d/%Y")
+hist <- hist[!is.na(hist$MEAN_TEMPERATURE),]
+hist$JULIAN <- as.numeric(format(hist$LOCAL_DATE, "%j"))
+
+#to avoid disproportional effects caused by extreme minimum temperatures, 
+#we subset and use the summer months only
+histsummer <- subset(hist, LOCAL_MONTH>=6 & LOCAL_MONTH<=8)
+
+#create list to separate each year
+year_list <- split(histsummer, f = histsummer$LOCAL_YEAR)
+all_results <- list()
+
+#loop to run linear model to detrend summer temperatures in each year
+count = 1
+for (i in year_list) {
+  
+  #fit a linear model
+  linear <- lm((i[, 15]-i[, 13]) ~ i[, 37])
+  
+  #save detrended data for each year
+  result <- residuals(linear) + mean(i[, 15]-i[, 13])
+  all_results[[count]] = as.data.frame(result)
+  count=count+1
+  
+}
+
+#create dataframe based on detrended data
+MAXMIN=unlist(all_results)
+MAXMIN <- as.data.frame(MAXMIN)
+MAXMIN$LOCAL_YEAR <- histsummer$LOCAL_YEAR
+MAXMIN$JULIAN <- histsummer$JULIAN
+
+#run gam model to check if max-min amplitude has reduced across years
+library(mgcv)
+gam.model <- gam(MAXMIN ~ s(JULIAN,bs="tp")+s(LOCAL_YEAR,bs="tp",k=30), data=MAXMIN, method="REML")
+par(mfrow = c(2, 2))
+plot(gam.model)
+summary(gam.model)
+gam.check(gam.model)
+
+
