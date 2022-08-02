7f2daa3714c284d428da8cf7a0b09a3980875e9f
Debora <91338311+d4andrad@users.noreply.github.com>
2022-08-02 11:32:27 -0400

    Update max-min_temps_historical_toronto.R

--- historical_temps_analyses/max-min_temps_historical_toronto.R
+++ historical_temps_analyses/max-min_temps_historical_toronto.R
@@ -185,8 +185,9 @@ gam.check(gam.model)
 summary(gam.model)
 
 #edf in GAM models:
-#"The effective degrees of freedom (edf) estimated from generalized additive models were used as a proxy for the degree of non-linearity in stressor-response 
-#relationships. (a) An edf of 1 is equivalent to a linear relationship, (b) an edf > 1 and ≤ 2 is a weakly non-linear relationship, and (c) an edf > 2 indicates 
+#The effective degrees of freedom (edf) estimated from generalized additive models can be used as a proxy for the degree of non-linearity in the relationship
+#predictor x response. 
+#"(a) An edf of 1 is equivalent to a linear relationship, (b) an edf > 1 and ≤ 2 is a weakly non-linear relationship, and (c) an edf > 2 indicates 
 #a highly non-linear relationship" (Zuur et al. 2009).
 
 #plot actual and predicted data
